d8371a3d35a77ebd4d810a01409c97fee51d94fd
hoge <2104240033yt@edu.oca.ac.jp>
2023-05-20 15:15:31 +0900

    5/20

--- test_selenium.py
+++ test_selenium.py
@@ -8,6 +8,24 @@ option.add_argument('--headless')
 
 driver = webdriver.Chrome(options=option)
 driver.get('https://www.google.co.jp/')
+driver.get('https://www.google.co.jp/')
+
+# 検索フィールドの取得
+query = driver.find_element(by=By.NAME, value='q')
+
+# 検索文字列を入力
+query.send_keys('バナナはおやつに入りますか')
+
+# 3秒待つ
+time.sleep(3)
+
+# 検索ボタンをクリック
+button = driver.find_element(by=By.NAME, value='btnK')
+button.click()
+
+# 3秒待つ
+time.sleep(3)
+
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
