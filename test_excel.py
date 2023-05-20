711663088860801e29227096e5601e775efcfcd7
hoge <2104240033yt@edu.oca.ac.jp>
2023-05-20 17:02:12 +0900

    5/20

--- test_excel.py
+++ test_excel.py
@@ -14,5 +14,16 @@ ws.append([1, 2, 3])
 import datetime
 ws['A2'] = datetime.datetime.now()
 
+# 10行×10列にインクリメントした数字を設定
+
+current_number = 1
+
+for row in range(1, 11):
+    for col in range(1, 11):
+        ws.cell(row=row, column=col).value = current_number
+        current_number += 1
+
 # Save the file
 wb.save("sample.xlsx")
+
+ws.create_sheet()
